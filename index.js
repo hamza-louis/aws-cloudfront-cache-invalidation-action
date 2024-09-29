45a94b153ed6278f8f046c8f5ad7714d04eb2a14
Hamza Louis <h.louis@bankaletihad.com>
2024-09-29 23:37:45 +0300

    initial push of the index.js code with its package.json

---
+++ index.js
@@ -0,0 +1,55 @@
+const { CloudFrontClient, CreateInvalidationCommand , GetInvalidationCommand } = require("@aws-sdk/client-cloudfront"); 
+
+const cloudfront = new CloudFrontClient({region: process.env.AWS_REGION});
+
+
+const checkCFCacheInvalidationStatus = async (CFDistributionID, CFInvalidationID) => {
+    let status = 'InProgress';
+    while (status === 'InProgress') {
+      const invalidationResult = await cloudfront.send(new GetInvalidationCommand({
+        DistributionId: CFDistributionID,
+        Id: CFInvalidationID,
+      }));
+  
+      status = invalidationResult.Invalidation.Status;
+      console.log(`Current status: ${status}`);
+      
+      console.log(`Waiting for 5 seconds before re-checking the status ...`)
+      await new Promise(resolve => setTimeout(resolve, 3000));
+    }
+
+    return status;
+};
+
+const invalidateCFCacheAndWait = async (CFDistributionID, CFInvalidationPaths) => {
+    CFDistributionID=process.env.CF_DISTRIBUTION_ID
+    CFInvalidationPaths=process.env.CF_INVALIDATION_PATHS
+    const invalidationRequestBody = {
+        DistributionId: CFDistributionID, 
+        InvalidationBatch: { 
+          Paths: {
+            Quantity: CFInvalidationPaths.length, 
+            Items: CFInvalidationPaths
+            },
+          CallerReference: `${Date.now()}`, 
+        },
+    };
+    try {
+        console.log(`Creating invalidation for CloudFront distribution => ${CFDistributionID} ...`)
+        const invalidationResponse = await cloudfront.send(new CreateInvalidationCommand(invalidationRequestBody));
+        var CFInvalidationID = invalidationResponse.Invalidation.Id;
+        console.log(`Invalidation created: ${CFInvalidationID}`);
+    } catch (error) {
+        console.error('Error creating or checking invalidation:', error);
+    }
+
+    try {
+        console.log(`Checking the status of the invalidation ${CFInvalidationID} for the CloudFront distribution => ${CFDistributionID} ...`)
+        const CFInvalidationStatus = await checkCFCacheInvalidationStatus(CFDistributionID, CFInvalidationID);
+        console.log(`Invalidation completed: ${CFInvalidationStatus}`);
+    } catch (error) {
+        console.error('Error creating or checking invalidation:', error);
+    }
+}
+
+invalidateCFCacheAndWait();
